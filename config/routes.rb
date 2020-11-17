66103efe2be04e97b9687f47ca87927ea88d5bf3
Paul Hemmingson <paul-hemmingson@t-online.de>
2020-11-17 17:26:41 +0100

    SHOW ALL Page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :workshops
+  get 'index' => 'workshops#index'
+  get 'show' => 'workshops#show'
   # other routes are nested inside :workshops
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
