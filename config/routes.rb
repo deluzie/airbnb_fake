34759f5bf8e382b03c460c2f13284f5b637a9946
Paul Brar <ibrar@ualberta.ca>
2020-11-19 17:07:07 +0100

    routed action to confirm booking and related button action

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   resources :workshops do
     resources :bookings, only: [:new, :create]
   end
-  resources :bookings, only: [:index]
-  get '/requests', to: 'workshops#requests'
+  resources :bookings, only: [:index, :update]
+  get '/requests', to: 'workshops#requests', as: :requests
+  # patch '/requests/confirm', to: 'workshops#confirm', as: :confirm_request
   # other routes are nested inside :workshops
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
