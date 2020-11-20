00b98de26a715cc0631200ca16bc4cbe8981f971
Paul Brar <ibrar@ualberta.ca>
2020-11-20 12:22:44 +0100

    potential fix take 2

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,3 +1,15 @@
 import flatpickr from "flatpickr";
 
-flatpickr(".datepicker", {});
+
+// flatpickr(".datepicker", {});
+
+document.addEventListener('turbolinks:load', () => {
+  flatpickr(".datepicker", {
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d"
+  })
+});
+
+
+// comment for test
